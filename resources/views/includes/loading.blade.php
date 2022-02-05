6791dfc54b20cf269c28c3b91ac39457f7f0c9b5
unknown <ioko.peezaza@gmail.com>
2022-02-05 22:48:33 +0700

    5-2-65

--- resources/views/includes/loading.blade.php
+++ resources/views/includes/loading.blade.php
@@ -4,7 +4,7 @@
     <div class="loading-content">
         <div class="loading-circle"></div>
         <div class="d-flex justify-content-center align-items-center w-100 h-100">
-            <img class="loading-image" src="{{ asset('images/loading.png') }}" alt="loader">
+            <img class="loading-image" src="{{ secure_asset('images/loading.png') }}" alt="loader">
         </div>
     </div>
 </div>

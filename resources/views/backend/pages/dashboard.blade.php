b7597c6188e63a6bd834779e53a2b123561dac63
Your Name <ioko.peezaza@gmail.com>
2023-07-12 15:58:17 +0700

    commit

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -220,9 +220,8 @@
         <div class="col-lg-3 col-md-4 col-sm-6">
             <div class="card card-success">
                 <div class="card-header rounded-0">
-                    <h3 class="card-title"><i class="fas fa-book"></i> บทความ<br /><span class="text-sm">(ไม่ซ้ำกัน (มี
-                            WORD
-                            และ PDF))</span></h3>
+                    <h3 class="card-title"><i class="fas fa-book"></i> บทความ<br /><span class="text-sm">(ไม่ซ้ำกัน
+                            มี WORD และ PDF)</span></h3>
 
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -327,7 +326,7 @@
             <div class="card card-success">
                 <div class="card-header rounded-0">
                     <h3 class="card-title"><i class="fas fa-chart-area"></i> กราฟบทความ<br /><span
-                            class="text-sm">(ไม่ซ้ำกัน (มี WORD และ PDF))</span>
+                            class="text-sm">(ไม่ซ้ำกัน มี WORD และ PDF)</span>
                     </h3>
 
                     <div class="card-tools">
